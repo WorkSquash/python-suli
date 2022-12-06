2b8003666c7cf83d53b127098ea36f97c041bde8
WorkSquash <107418778+WorkSquash@users.noreply.github.com>
2022-12-06 14:25:37 +0100

    Egész számok itt vannak!

--- 11-24/random-egész-szám.py
+++ 11-24/random-egész-szám.py
@@ -1,9 +1,10 @@
 import random 
 
-szam=round(random.uniform(0, 10), 2)
+szam=random.randint(0,100)
+input('Ha egynél nagyobb számot írsz be, akkor a játék automatikusan véget ér.')
 
 pro=0
-input('Ha egynél nagyobb számot írsz be, akkor a játék automatikusan véget ér.')
+
 
 csal=int(input('Csalások bekapcsolása? [0-1]'))
 
@@ -14,39 +15,38 @@ while pro!=1:
     if csal==1 and csal!=0:
         print('Csalás bekapcsolva.')
         print(szam)
-
         pro=int(input('Adja meg a próbálkozások számát!:'))
-
-        tipp=float(input('Adja meg a tippjét! [0-10] (PL:9.99)'))
-
+        
+        tipp=int(input('Adja meg a tippjét! [0-100] (Pl:36)'))
+        
         while pro !=1:
             if tipp>szam:
                 pro=pro-1
                 print('A tipp nagyobb mint a szám!')
                 print('Probáld újra')
                 print(pro,'próbálkozása maradt!')
-                tipp=float(input('Adja meg a tippjét! [0-10] (PL:9.99)'))
+                tipp=int(input('Adja meg a tippjét! [0-100] (Pl:36)'))
         
             elif szam>tipp:
                 pro=pro-1
                 print('A tipp kisebb mint a szám!')
                 print('Probáld újra')
                 print(pro,'próbálkozása maradt!')
-                tipp=float(input('Adja meg a tippjét! [0-10] (PL:9.99)'))
+                tipp=int(input('Adja meg a tippjét! [0-100] (Pl:36)'))
         
         
 
             else:
                 print('Eltaláltad!')
                 pro=1
-                #print('A szám', szam, 'volt!')
+                print('A szám', szam, 'volt!')
 
     elif csal==0 and csal!=1:
         print('Csalás kikapcsolva.')
-
-        pro=int(input('Adja meg a próbálkozások számát!:'))
         
-        tipp=float(input('Adja meg a tippjét! [0-10] (PL:9.99)'))
+        pro=int(input('Adja meg a próbálkozások számát!:'))
+
+        tipp=int(input('Adja meg a tippjét! [0-100] (Pl:36)'))
 
         while pro !=1:
             if tipp>szam:
@@ -54,14 +54,14 @@ while pro!=1:
                 print('A tipp nagyobb mint a szám!')
                 print('Probáld újra')
                 print(pro,'próbálkozása maradt!')
-                tipp=float(input('Adja meg a tippjét! [0-10] (PL:9.99)'))
+                tipp=int(input('Adja meg a tippjét! [0-100] (Pl:36)'))
         
             elif szam>tipp:
                 pro=pro-1
                 print('A tipp kisebb mint a szám!')
                 print('Probáld újra')
                 print(pro,'próbálkozása maradt!')
-                tipp=float(input('Adja meg a tippjét! [0-10] (PL:9.99)'))
+                tipp=int(input('Adja meg a tippjét! [0-100] (Pl:36)'))
         
         
 
